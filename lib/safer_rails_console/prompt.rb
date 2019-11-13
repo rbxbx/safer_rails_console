fa6c86aeed7f20cc18b41d993bcfe178a493b986
Robert Pitts <rbxbxdev@gmail.com>
2019-11-13 12:03:25 -0500

    Unscope `Pry` references

--- lib/safer_rails_console/prompt.rb
+++ lib/safer_rails_console/prompt.rb
@@ -3,10 +3,10 @@ module SaferRailsConsole
     class << self
       def formatted_env
         if Rails.env.production?
-          bold_env = Pry::Helpers::Text.bold(Rails.env)
-          Pry::Helpers::Text.red(bold_env)
+          bold_env = ::Pry::Helpers::Text.bold(Rails.env)
+          ::Pry::Helpers::Text.red(bold_env)
         elsif Rails.env.development?
-          Pry::Helpers::Text.green(Rails.env)
+          ::Pry::Helpers::Text.green(Rails.env)
         else
           Rails.env
         end
@@ -24,18 +24,18 @@ module SaferRailsConsole
 
   desc = "Includes the current Rails environment and project folder name.\n" \
           "[1] [project_name][Rails.env] pry(main)>"
-  if Pry::Prompt.respond_to?(:add)
-    Pry::Prompt.add 'rails', desc, %w(> *) do |target_self, nest_level, pry, sep|
+  if ::Pry::Prompt.respond_to?(:add)
+    ::Pry::Prompt.add 'rails', desc, %w(> *) do |target_self, nest_level, pry, sep|
       "[#{pry.input_ring.size}] " \
       "[#{Prompt.project_name}][#{Prompt.formatted_env}] " \
-      "#{pry.config.prompt_name}(#{Pry.view_clip(target_self)})" \
+      "#{pry.config.prompt_name}(#{::Pry.view_clip(target_self)})" \
       "#{":#{nest_level}" unless nest_level.zero?}#{sep} "
     end
   else
     draw_prompt = lambda do |target_self, nest_level, pry, sep|
       "[#{pry.input_array.size}] " \
       "[#{Prompt.project_name}][#{Prompt.formatted_env}] " \
-      "#{pry.config.prompt_name}(#{Pry.view_clip(target_self)})" \
+      "#{pry.config.prompt_name}(#{::Pry.view_clip(target_self)})" \
       "#{":#{nest_level}" unless nest_level.zero?}#{sep} "
     end
     prompts = [
@@ -46,6 +46,6 @@ module SaferRailsConsole
         draw_prompt.call(target_self, nest_level, pry, '*')
       end
     ]
-    Pry::Prompt::MAP["rails"] = {value: prompts, description: desc}
+    ::Pry::Prompt::MAP["rails"] = {value: prompts, description: desc}
   end
 end
